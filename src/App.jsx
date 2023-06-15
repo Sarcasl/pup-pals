f2438fe736ef7b7fe8872ba82b1109c295ebd1cb
Sarcasl <venwes@gmail.com>
2023-06-15 16:09:57 -0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+
+<h1>Puppy Pals</h1>
+
 function App() {
-
-
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
+    <div id="puppy">
       <div>
+        {
+          puppies.map((puppy) => 
+          {
+            function handleClick (){
+            setFeatPupId (puppy.id) 
+            }
+            return <p key={puppy.id} onClick={handleClick}>{puppy.name}</p>
+          })
+        }</div>
         
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Emai: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )
+        }
       </div>
-    
   )
 }
 
